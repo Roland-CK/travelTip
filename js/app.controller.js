b5e7f569ee73b958116d3e91595c9dc658e04458
Dan Katorza <dkatorza@gmail.com>
2021-08-18 19:07:18 +0300

    added getLocation , gLocation, addLocation

--- js/app.controller.js
+++ js/app.controller.js
@@ -1,18 +1,42 @@
 import { locService } from './services/loc.service.js'
 import { mapService } from './services/map.service.js'
-
+console.log('controller');
 window.onload = onInit;
 window.onAddMarker = onAddMarker;
 window.onPanTo = onPanTo;
 window.onGetLocs = onGetLocs;
 window.onGetUserPos = onGetUserPos;
+window.renderLocations = renderLocations;
+window.onDelete = onDelete;
 
 function onInit() {
+    renderLocations()
     mapService.initMap()
         .then(() => {
             console.log('Map is ready');
+
         })
         .catch(() => console.log('Error: cannot init map'));
+}
+
+
+function renderLocations() {
+    const locations = locService.getLocations()
+    const strHtmls = locations.map((location, idx) => {
+        return `
+            <tr>
+                <td>${idx + 1}</td>
+                <td>${location.name}</td>
+                <td>${location.lat}</td>
+                <td>${location.lng}</td>
+                <td class="go-td"><button class="go-btn" data-lat="${location.lat
+                 }" data-lng="${location.lng}" >Go</button></td>
+                <td><button class="delete-btn" data-idx="${idx}" onclick="onDelete(this.dataset.idx)">Delete</button></td>
+            </tr>
+          `;
+
+    });
+    document.querySelector('.locations-data').innerHTML = strHtmls.join('');
 }
 
 // This function provides a Promise API to the callback-based-api of getCurrentPosition
@@ -29,7 +53,7 @@ function onAddMarker() {
 }
 
 function onGetLocs() {
-    locService.getLocs()
+    locService.getLocations()
         .then(locs => {
             console.log('Locations:', locs)
             document.querySelector('.locs').innerText = JSON.stringify(locs)
@@ -50,4 +74,9 @@ function onGetUserPos() {
 function onPanTo() {
     console.log('Panning the Map');
     mapService.panTo(35.6895, 139.6917);
+}
+
+function onDelete(idx) {
+    locService.deleteLocation(idx);
+    renderLocations();
 }
--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,19 +1,52 @@
+import {storageService} from './storage.service.js';
+
+const KEY = 'locationDB';
+
+
 export const locService = {
-    getLocs
+    getLocations,
+    addLocation,
+    deleteLocation,
 }
 
 
-const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
-    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
+// const locs = [
+//     { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
+//     { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
+// ]
+
+
+const gLocations = [
+    {
+        lat:31.781646776490927,
+        lng:34.65058981550658,
+        name:'Dan',
+        createdAt:'',
+        updatedAt:'',
+    },
 ]
 
-function getLocs() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(locs);
-        }, 2000)
-    });
+function getLocations() {
+    return gLocations;
+}
+
+function addLocation(lat, lng, name){
+    //Don't forget to check if user did not entred name...
+    gLocations.push(_createLocation(lat, lng, name));
+    return gLocations;
 }
 
 
+function _createLocation(name,lat,lng) {
+    return {
+        lat,
+        lng,
+        name,
+        createdAt,
+        updatedAt,
+    };
+}
+
+function deleteLocation(idx) {
+    gLocations.splice(idx, 1);
+}
